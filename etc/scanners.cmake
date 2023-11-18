ea07eba53e0e6460228e615be69754204284be00
Dang Truong <truongminhdang26403@gmail.com>
2023-11-17 19:47:07 -0500

    Use clang-format-17 and clang-tidy-17

--- etc/scanners.cmake
+++ etc/scanners.cmake
@@ -2,14 +2,14 @@ file (GLOB_RECURSE MINNOW_CC_FILES ${CMAKE_SOURCE_DIR}/src/*.cc)
 
 file (GLOB_RECURSE ALL_SRC_FILES *.hh *.cc)
 
-add_custom_target (format "clang-format" -i ${ALL_SRC_FILES} COMMENT "Formatting source code...")
+add_custom_target (format "clang-format-17" -i ${ALL_SRC_FILES} COMMENT "Formatting source code...")
 
 foreach (tidy_target ${ALL_SRC_FILES})
   get_filename_component (basename ${tidy_target} NAME)
   get_filename_component (dirname ${tidy_target} DIRECTORY)
   get_filename_component (basedir ${dirname} NAME)
   set (tidy_target_name "${basedir}__${basename}")
-  set (tidy_command clang-tidy --quiet -header-filter=.* -p=${PROJECT_BINARY_DIR} ${tidy_target})
+  set (tidy_command clang-tidy-17 --quiet -header-filter=.* -p=${PROJECT_BINARY_DIR} ${tidy_target})
   add_custom_target (tidy_${tidy_target_name} ${tidy_command})
   list (APPEND ALL_TIDY_TARGETS tidy_${tidy_target_name})
 
